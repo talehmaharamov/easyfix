492e9394f0bbadad3a60625c7cb30683828bfa8d
Taleh Maharramov <taleh.maharramov@eco.gov.az>
2024-09-03 08:08:08 +0400

    second commit

---
+++ resources/views/frontend/includes/footer.blade.php
@@ -0,0 +1,195 @@
+<div class="rts-footer-one footer-bg-one mt--160 pb--85">
+    <div class="container">
+        <div class="row g-0 bg-cta-footer-one">
+            <div class="col-lg-12">
+                <div class="bg-cta-footer-one wrapper">
+                    <div class="row align-items-center">
+                        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                            <a href="#" class="logo-area-footer">
+                                <img src="{{ asset('images/logo-t.png') }}" style="width: 50%;" alt="logo">
+                            </a>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                            <div class="single-cta-area">
+                                <div class="icon">
+                                    <i class="fa-solid fa-phone"></i>
+                                </div>
+                                <div class="contact-info">
+                                    <p>Phone Number</p>
+                                    <a href="tel:{{ settings('phone') }}">
+                                        {{ settings('phone') }}
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                            <div class="single-cta-area">
+                                <div class="icon">
+                                    <i class="fa-solid fa-envelope"></i>
+                                </div>
+                                <div class="contact-info">
+                                    <p>Email Us Here</p>
+                                    <a href="mailto:{{ settings('email') }}">
+                                        {{ settings('email') }}
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+                            <div class="single-cta-area last">
+                                <div class="icon">
+                                    <i class="fa-solid fa-phone"></i>
+                                </div>
+                                <div class="contact-info">
+                                    <p>Office Address</p>
+                                    <a href="https://www.google.com/maps" target="_blank">251 Hilton, Berlin DE</a>
+                                </div>
+                            </div>
+                            <!-- single contact area end -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="row pt--90">
+            <div class="col-lg-12">
+                <div class="single-footer-one-wrapper">
+                    <div class="single-footer-component first">
+                        <div class="title-area">
+                            <h5 class="title">About Company</h5>
+                        </div>
+                        <div class="body">
+                            <p class="disc">
+                                Centric aplications productize before front end vortals visualize front end is results and value added
+                            </p>
+                            <div class="rts-social-style-one">
+                                <ul>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa-brands fa-facebook-f"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa-brands fa-twitter"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa-brands fa-youtube"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa-brands fa-linkedin-in"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="single-footer-component">
+                        <div class="title-area">
+                            <h5 class="title">Useful Links</h5>
+                        </div>
+                        <div class="body">
+                            <div class="pages-footer">
+                                <ul>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa-solid fa-arrow-right"></i>
+                                            <p>About Us</p>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa-solid fa-arrow-right"></i>
+                                            <p>Our Gallery</p>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa-solid fa-arrow-right"></i>
+                                            <p>Our Services</p>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa-solid fa-arrow-right"></i>
+                                            <p>Our Team</p>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa-solid fa-arrow-right"></i>
+                                            <p>Contact Us</p>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="single-footer-component">
+                        <div class="title-area">
+                            <h5 class="title">What We Do</h5>
+                        </div>
+                        <div class="body">
+                            <div class="pages-footer">
+                                <ul>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa-solid fa-arrow-right"></i>
+                                            <p>Our Service</p>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa-solid fa-arrow-right"></i>
+                                            <p>Office Service</p>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa-solid fa-arrow-right"></i>
+                                            <p>Industry Service</p>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa-solid fa-arrow-right"></i>
+                                            <p>Private Service</p>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa-solid fa-arrow-right"></i>
+                                            <p>Single Service</p>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="single-footer-component last">
+                        <div class="title-area">
+                            <h5 class="title">Photo Gallery</h5>
+                        </div>
+                        <div class="body">
+                            <div class="gallery-footer">
+                                <ul>
+                                    <li><a href="#"><img src="assets/images/footer/gallery/01.png" alt="gallery"></a></li>
+                                    <li><a href="#"><img src="assets/images/footer/gallery/02.png" alt="gallery"></a></li>
+                                    <li><a href="#"><img src="assets/images/footer/gallery/03.png" alt="gallery"></a></li>
+                                    <li><a href="#"><img src="assets/images/footer/gallery/04.png" alt="gallery"></a></li>
+                                    <li><a href="#"><img src="assets/images/footer/gallery/05.png" alt="gallery"></a></li>
+                                    <li><a href="#"><img src="assets/images/footer/gallery/06.png" alt="gallery"></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/frontend/includes/scripts.blade.php
@@ -0,0 +1,63 @@
+<div class="search-input-area">
+    <div class="container">
+        <div class="search-input-inner">
+            <div class="input-div">
+                <input id="searchInput1" class="search-input" type="text" placeholder="Search by keyword or #">
+                <button><i class="far fa-search"></i></button>
+            </div>
+        </div>
+    </div>
+    <div id="close" class="search-close-icon"><i class="far fa-times"></i></div>
+</div>
+
+<div id="anywhere-home" class="">
+</div>
+
+
+<div class="progress-wrap">
+    <svg class="progress-circle svg-content" width="100%" height="100%" viewBox="-1 -1 102 102">
+        <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" style="transition: stroke-dashoffset 10ms linear 0s; stroke-dasharray: 307.919, 307.919; stroke-dashoffset: 307.919;"></path>
+    </svg>
+</div>
+
+<div id="elevate-load">
+    <div class="loader-wrapper">
+        <div class="lds-ellipsis">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+</div>
+
+
+<script src="{{ asset('frontend/js/plugins/jquery.min.js')}}"></script>
+<script src="{{ asset('frontend/js/vendor/jqueryui.js')}}"></script>
+<script src="{{ asset('frontend/js/plugins/counter-up.js')}}"></script>
+<script src="{{ asset('frontend/js/plugins/swiper.js')}}"></script>
+
+
+{{--<!-- <script src="{{ asset('frontend/js/vendor/twinmax.js')}}"></script> -->--}}
+{{--<!-- <script src="{{ asset('frontend/js/vendor/split-text.js')}}"></script> -->--}}
+{{--<!-- <script src="{{ asset('frontend/js/plugins/text-plugins.js')}}"></script> -->--}}
+<script src="{{ asset('frontend/js/plugins/metismenu.js')}}"></script>
+
+
+<script src="{{ asset('frontend/js/vendor/waypoint.js')}}"></script>
+<script src="{{ asset('frontend/js/vendor/waw.js')}}"></script>
+
+
+<script src="{{ asset('frontend/js/plugins/gsap.min.js')}}"></script>
+<script src="{{ asset('frontend/js/plugins/scrolltigger.js')}}"></script>
+{{--<!-- <script src="{{ asset('frontend/js/plugins/aos.js')}}"></script> -->--}}
+{{--<!-- <script src="{{ asset('frontend/js/plugins/jquery-ui.js')}}"></script> -->--}}
+<script src="{{ asset('frontend/js/plugins/jquery-timepicker.js')}}"></script>
+{{--<!-- <script src="{{ asset('frontend/js/vendor/sal.min.js')}}"></script> -->--}}
+
+<script src="{{ asset('frontend/js/plugins/bootstrap.min.js')}}"></script>
+{{--<!-- <script src="{{ asset('frontend/js/plugins/jquery-slideNav.js')}}"></script> -->--}}
+{{--<!-- <script src="{{ asset('frontend/js/plugins/hover-revel.js')}}"></script> -->--}}
+{{--<!-- <script src="{{ asset('frontend/js/plugins/contact-form.js')}}"></script> -->--}}
+
+<script src="{{ asset('frontend/js/main.js')}}"></script>
